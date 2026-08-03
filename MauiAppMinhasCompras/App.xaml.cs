2c948764e48ce74d15677d7107528dcf2112c6fb
Paulo Benetello <paulohbenetello@gmail.com>
2026-08-03 09:52:10 -0300

    add models e troca de tela inicial

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -7,11 +7,12 @@ namespace MauiAppMinhasCompras
         public App()
         {
             InitializeComponent();
+            MainPage = new NavigationPage(new Views.ListaProduto());
         }
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
-        }
+        //protected override Window CreateWindow(IActivationState? activationState)
+        //{
+        //    return new Window(new AppShell());
+        //}
     }
 }
